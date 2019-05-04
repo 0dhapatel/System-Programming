5e3edf1beec82c8c0f4780edda6fa28be13dedf8
0dhapatel <39307304+0dhapatel@users.noreply.github.com>
2019-05-04 17:22:23 -0400

    Update WTFserver.c

--- Asst3/WTFserver.c
+++ Asst3/WTFserver.c
@@ -87,53 +87,39 @@ int main(int argc, char**argv)
         fprintf(stderr, "Port No not provideed\n");
         exit(1);
     }
-    int sockfd, newsockfd, portno, in;
-    char buffer[255];
-    struct sockaddr_in serv_addr, cli_addr;
-    socklen_t clilen;
     
-    sockfd=socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd<0){
-        error("Cannot open Socket");
-    }
     
-    bzero((char*) &serv_addr, sizeof(serv_addr));
-    portno= atoi(argv[1]);
-    serv_addr.sin_family=AF_INET;
-    serv_addr.sin_addr_.s_addr=INADDR_ANY;
-    serv_addr.port=htons(portno);
-    
-    if(blind(sockfd,(struct sockaddr*) &serv_addr, sizeof(serv_addr))<0){
-        error("Binding Failed");
-    }
-    
-    listen(sockfd,5);
-    clilen=sizeof(cli_addr);
-    
-    newsockfd=accept(sockfd,(struct sockaddr*) &cli_addr,&clilen);
-    
-    if(newsockfd<0){
-        error("Error on accept");
-    }
-    while(1){
-        bzero(buffer,256);
-        in=read(newsockfd,buffer,255);
-        if(in<0){
-            error("Error on reading");
-        }
-        printf("Client: %s\n", buffer);
-        bzero(buffer,255);
-        fgets(buffer,255,stdin);
-        
-        in=write (newsockfd,buffer,strlen(buffer));
-        if(in<0){
-            error("Error on writing");
-        }
-        int i=strncmp(SIGINT,buffer,2);
-        if(i==0){
-            break;
-        }
-    }
+    int sockfd, newsockfd, portno, clilen;
+     char buffer[256];
+     struct sockaddr_in serv_addr, cli_addr;
+     int n;
+     if (argc < 2) {
+         fprintf(stderr,"ERROR, no port provided\n");
+         exit(1);
+     }
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0) 
+        error("ERROR opening socket");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0) 
+              error("ERROR on binding");
+     listen(sockfd,5);
+     clilen = sizeof(cli_addr);
+     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+     if (newsockfd < 0) 
+          error("ERROR on accept");
+     bzero(buffer,256);
+     n = read(newsockfd,buffer,255);
+     if (n < 0) error("ERROR reading from socket");
+     printf("Here is the message: %s\n",buffer);
+     n = write(newsockfd,"I got your message",18);
+     if (n < 0) error("ERROR writing to socket");
+     return 0; 
     
     close(newsockfd);
     close(sockfd);
