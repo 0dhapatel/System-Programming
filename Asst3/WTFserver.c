31ac28392ab53bd5ba5a1e977de3a0783201cfd8
0dhapatel <39307304+0dhapatel@users.noreply.github.com>
2019-05-04 20:24:52 -0400

    Update WTFserver.c

--- Asst3/WTFserver.c
+++ Asst3/WTFserver.c
@@ -1,15 +1,52 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
+#include <pthread.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <linux/in.h>
+#include <unistd.h>
 
-void error(const char* msg)
+typedef struct
 {
-    perror(msg);
-    exit(1);
+	int sock;
+	struct sockaddr address;
+	int addr_len;
+} connection_t;
+
+void * process(void * ptr)
+{
+	char * buffer;
+	int len;
+	connection_t * conn;
+	long addr = 0;
+
+	if (!ptr) pthread_exit(0); 
+	conn = (connection_t *)ptr;
+
+	/* read length of message */
+	read(conn->sock, &len, sizeof(int));
+	if (len > 0)
+	{
+		addr = (long)((struct sockaddr_in *)&conn->address)->sin_addr.s_addr;
+		buffer = (char *)malloc((len+1)*sizeof(char));
+		buffer[len] = 0;
+
+		/* read message */
+		read(conn->sock, buffer, len);
+
+		/* print message */
+		printf("%d.%d.%d.%d: %s\n",
+			(int)((addr      ) & 0xff),
+			(int)((addr >>  8) & 0xff),
+			(int)((addr >> 16) & 0xff),
+			(int)((addr >> 24) & 0xff),
+			buffer);
+		free(buffer);
+	}
+
+	/* close socket and clean up */
+	close(conn->sock);
+	free(conn);
+	pthread_exit(0);
 }
 /*.    create and remove files and directories   .*/
 // have to pass strings to client
@@ -81,47 +118,71 @@ void removefile( char* dir, char* act) //removes when nothing in the folder
 
 /**/
 
-int main(int argc, char**argv)
+int main(int argc, char ** argv)
 {
-    if(argc<2){
-        fprintf(stderr, "Port No not provideed\n");
-        exit(1);
-    }
-    
-    
-    int sockfd, newsockfd, portno, clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
-        error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     return 0; 
-    
-    close(newsockfd);
-    close(sockfd);
-    return 0;
+	int sock = -1;
+	struct sockaddr_in address;
+	int port;
+	connection_t * connection;
+	pthread_t thread;
+
+	/* check for command line arguments */
+	if (argc != 2)
+	{
+		fprintf(stderr, "usage: %s port\n", argv[0]);
+		return -1;
+	}
+
+	/* obtain port number */
+	if (sscanf(argv[1], "%d", &port) <= 0)
+	{
+		fprintf(stderr, "%s: error: wrong parameter: port\n", argv[0]);
+		return -2;
+	}
+
+	/* create socket */
+	sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sock <= 0)
+	{
+		fprintf(stderr, "%s: error: cannot create socket\n", argv[0]);
+		return -3;
+	}
+
+	/* bind socket to port */
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(port);
+	if (bind(sock, (struct sockaddr *)&address, sizeof(struct sockaddr_in)) < 0)
+	{
+		fprintf(stderr, "%s: error: cannot bind socket to port %d\n", argv[0], port);
+		return -4;
+	}
+
+	/* listen on port */
+	if (listen(sock, 5) < 0)
+	{
+		fprintf(stderr, "%s: error: cannot listen on port\n", argv[0]);
+		return -5;
+	}
+
+	printf("%s: ready and listening\n", argv[0]);
+	
+	while (1)
+	{
+		/* accept incoming connections */
+		connection = (connection_t *)malloc(sizeof(connection_t));
+		connection->sock = accept(sock, &connection->address, &connection->addr_len);
+		if (connection->sock <= 0)
+		{
+			free(connection);
+		}
+		else
+		{
+			/* start a new thread but do not wait for it */
+			pthread_create(&thread, 0, process, (void *)connection);
+			pthread_detach(thread);
+		}
+	}
+	
+	return 0;
 }
