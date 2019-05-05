43a0acef4e3218932f4ba43e6e01c2e6b39bb3c8
0dhapatel <39307304+0dhapatel@users.noreply.github.com>
2019-05-04 21:40:42 -0400

    Update WTF.c

--- Asst3/WTF.c
+++ Asst3/WTF.c
@@ -3,6 +3,11 @@
 #include <netdb.h>
 #include <unistd.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 int main(int argc, char ** argv)
 {
